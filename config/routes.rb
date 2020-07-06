21d9d00a880b78cf73555f8f2ba8d414558b36f2
HARAYUKA <tukituki442@icloud.com>
2020-07-06 14:20:58 +0000

    勤怠変更申請完了

--- config/routes.rb
+++ config/routes.rb
@@ -11,20 +11,21 @@ Rails.application.routes.draw do
      member do
       get 'edit_basic_info'
       patch 'update_basic_info'
-      get 'attendances/edit_one_month'
-      patch 'attendances/update_one_month'
-      get 'confirm_one_month'
+      get 'confirm_one_month' # 勤怠変更申請送信
+      get 'instructor_apploval' # 所属長承認ボタン
+      get 'attendances/edit_one_month' # 1ヶ月の勤怠編集
+      patch 'attendances/update_one_month' # 1ヶ月の勤怠更新
     end
     resources :attendances, only: :update do
       member do
-        get 'edit_overwork_request'
-        patch 'update_overwork_request'
+        get 'edit_overwork_request' # 残業申請モーダル表示
+        patch 'update_overwork_request' # 残業申請モーダル更新
       end
       collection do
-        get 'edit_notice_overwork'
-        patch 'update_notice_overwork'
-        get 'change_att_request'
-        patch 'update_att_request'
+        get 'edit_notice_overwork' # 残業申請お知らせモーダル表示
+        patch 'update_notice_overwork' # 残業申請お知らせモーダル更新
+        get 'change_att_request' # 勤怠変更申請モーダル表示
+        patch 'update_att_request' # 勤怠変更申請モーダル更新
       end
     end
   end
